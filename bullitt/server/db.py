ac5237fb20ad8d2f0ad73bb1e04ecd545351731a
unknown <mmabey@EN4065914.fulton.ad.asu.edu>
2012-04-04 11:27:49 -0700

    Renamed sql.py to db.py and put the buiness logic class and the database class in the same file.

--- bullitt/server/db.py
+++ bullitt/server/db.py
@@ -3,18 +3,26 @@
 Created on Apr 2, 2012
 
 @author: mmabey
+
+For documentation on SQLAlchemy, visit: 
+    http://docs.sqlalchemy.org/en/latest/core/tutorial.html
 '''
 
 # Library imports
+import hashlib
+import json
 import uuid
 
 # Third-party libraries
-import sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Column, String, \
+                       Integer, Boolean
 
 # Local imports
 
 # Constants
 __all__ = ['ServerBiz']
+LOGGING = True
+
 
 class ServerBiz(object):
     '''
@@ -26,7 +34,7 @@ class ServerBiz(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self):
         '''
         
         '''
@@ -36,12 +44,20 @@ class ServerBiz(object):
         '''
         Given a file and client ID, returns the permissions, None if NULL.
         '''
+        # 
+        pass
+    
+    
+    def get_file_owner(self, file_id):
+        '''
+        For a given file, return the UUID of the owner client.
+        '''
         pass
     
     
     def get_file_peers(self, file_id):
         '''
-        For a given file, returns the list of clients with a copy of it.
+        For a given file, return the list of clients with a copy of it.
         '''
         pass
     
@@ -58,8 +74,18 @@ class ServerBiz(object):
         pass
     
     
-    def _add_file(self):
+    def publish_update(self, file_id, checksum):
         '''
+        Announce the given file has been updated to the new checksum.
+        '''
+        pass
+    
+    
+    def _add_file(self, client_id,):
+        '''
+        Specialized version of modify operation.
+        
+        Inserts a new entry in the database for the file specified
         '''
         pass
     
@@ -72,6 +98,9 @@ class ServerBiz(object):
     
     def _mod_file(self):
         '''
+        Update the file index with the newest version of the file.
+        
+        
         '''
         pass
     
@@ -110,21 +139,56 @@ class BullittSQL(object):
     '''
 
 
-    def __init__(self, params):
+    def __init__(self):
         '''
         '''
+        with open('server_config.json') as fin:
+            config = json.load(fin)
+        
+        self.metadata = MetaData()
+        self.metadata.bind = create_engine('%s%s://%s:%s@%s/%s' % \
+                                           (config['db_type'],
+                                            config['db_conn'] != '' and \
+                                            '+' + config['db_conn'] or '',
+                                            config['sql_user'],
+                                            config['sql_pass'],
+                                            config['db_url'],
+                                            config['db_name']),
+                                           echo=LOGGING)
+        self.file_table = None
+        self.perm_table = None
         self.create_tables()
     
     
-    def create_tables(self):
+    def create_tables(self, drop=False):
         '''
-        Creates (destructive) the database and tables for the system.
+        Creates the database and tables for the system.
         
-        This method should only be called when the system is first being 
-        initialized and when it is necessary to clear out the database
-        completely.  Make sure you really want to do this before calling it.
+        :param drop:
+        Defaults to False, drop the tables before adding them, effectively 
+        resetting the entire database.
         '''
-        pass
+        
+        # Define the tables for the database
+        self.file_table = Table('file_list', self.metadata,
+                                Column('file_id', String(36), primary_key=True,
+                                       nullable=False),
+                                Column('file_name', String(128),
+                                       nullable=False),
+                                Column('owner_id', String(36), nullable=False),
+                                Column('sha1', String(40), nullable=False))
+        self.perm_table = Table('permissions', self.metadata,
+                                Column('pk', Integer, primary_key=True),
+                                Column('file_id', String(36), nullable=False),
+                                Column('user_id', String(36), nullable=False),
+                                Column('read', Boolean, nullable=False),
+                                Column('write', Boolean, nullable=False),
+                                Column('owner', Boolean, nullable=False))
+        if drop:
+            # Drop all the tables associated with the metadata object
+            self.metadata.drop_all()
+        # Create the tables
+        self.metadata.create_all()
 
 
 
