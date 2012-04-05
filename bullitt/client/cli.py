373401a1da491e93c36a42eb4efff2b5506c2053
unknown <Justin@Pagtop2.(none)>
2012-04-05 14:57:51 -0700

    Add client CLI and daemon files

---
+++ bullitt/client/cli.py
@@ -0,0 +1,13 @@
+'''
+Created on Apr 4, 2012
+
+@author: Justin
+'''
+
+def print_main_menu():
+    '''
+    Prints main CLI menu for the server.
+    '''
+
+if __name__ == '__main__':
+    pass
---
+++ bullitt/client/deamon.py
@@ -0,0 +1,8 @@
+'''
+Created on Apr 4, 2012
+
+@author: Justin
+'''
+
+if __name__ == '__main__':
+    pass
