9e579833675a8ef7ed471c0779630d7d28ba3d18
Mike Mabey <mike611@gmail.com>
2012-04-05 13:39:08 -0700

    Completed database module (server.db.BullittSQL). Added module for interacting with the RabbitMQ server.

---
+++ bullitt/common/cuffrabbit.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+'''
+Created on Oct 26, 2011
+
+@author: mmabey
+'''
+
+
+# Library imports
+import json
+import threading
+import urllib
+
+# Third-party libraries
+from pika.adapters import SelectConnection, BlockingConnection
+import pika
+import pika.spec as spec
+
+# Constants
+EXCHANGE_TYPES = ('direct', 'topic', 'headers', 'fanout')
+DEBUG = False
+INFO = False
+
+
+class RabbitObj(object):
+    '''
+    This is the class inherited by all other objects that work with RabbitMQ
+    within CUFF.
+    '''
+
+
+    def __init__(self,
+                 host='localhost',
+                 port=spec.PORT,
+                 virtual_host='/',
+                 credentials=None,
+                 channel_max=0,
+                 frame_max=spec.FRAME_MAX_SIZE,
+                 heartbeat=False):
+        '''
+        Initializes all the connection parameters and stores them in a 
+        ConnectionParameters object at self.conn_params.
+        
+        Parameters:
+        - host: Hostname or IP Address to connect to, defaults to localhost.
+        - port: TCP port to connect to, defaults to 5672
+        - virtual_host: RabbitMQ virtual host to use, defaults to /
+        - credentials: A instance of a credentials class to authenticate with.
+          Defaults to PlainCredentials for the guest user.
+        - channel_max: Maximum number of channels to allow, defaults to 0 for 
+         None
+        - frame_max: The maximum byte size for an AMQP frame. Defaults to 
+         131072
+        - heartbeat: Turn heartbeat checking on or off. Defaults to False.
+        '''
+
+        # Send the values to a ConnectionParameters object, which has built-in
+        # type-checking
+        if isinstance(host, unicode): host = str(host)
+        self.conn_params = pika.ConnectionParameters(host, port, virtual_host,
+                                                    credentials, channel_max,
+                                                    frame_max, heartbeat)
+        self.connection = None
+        self.channel = None
+        self.init_callback = None
+        self.exchange = None
+        self.routing_key = None
+        self.queue_created = False
+        self.queue_name = None
+        self.no_ack = False
+        self._debug_prefix = ""
+
+        # The binding key is essentially the same thing as a routing key, only it is
+        # used by receivers instead of senders. We set it to None here so that we can
+        # detect on the fly if we have executed a queue_bind() for the binding key
+        # before we attempt to receive messages. This is also how we prevent senders
+        # from performing a queue_bind(), since they don't generally do that.
+        self.binding_key = None
+
+
+    def init_connection(self, callback, queue_name='', exchange='', exchange_type='topic', routing_key="#", blocking=False):
+        '''
+        Handles all connection, channel, etc. issues involved in fully 
+        connecting to the RabbitMQ server. The function 'callback' is stored
+        and called once all steps have successfully completed. Using this 
+        method forces all communication to go through the queue specified by 
+        'queue'.
+        '''
+        self.exchange = str(exchange)
+        self.init_callback = callback
+
+        if exchange_type not in EXCHANGE_TYPES:
+            raise ValueError("Exchange type must be one of: %s" % str(EXCHANGE_TYPES))
+        else:
+            self.ex_type = str(exchange_type)
+
+        if isinstance(queue_name, unicode):
+            queue_name = str(queue_name)
+        elif not isinstance(queue_name, str):
+            raise TypeError("Queue must be a str, got %s instead" % str(type(queue_name)))
+        self.queue_name = queue_name
+
+        if isinstance(routing_key, unicode):
+            routing_key = str(routing_key)
+        elif not isinstance(routing_key, str):
+            raise TypeError("Routing key must be a str, got %s instead" % str(type(queue_name)))
+        self.routing_key = routing_key
+
+        if DEBUG: print self._debug_prefix + "About to start connection...",
+
+        # From here, determine how to initialize the connection further by 
+        # what type of exchange was requested.
+        if self.ex_type == "topic":
+            self._init_topic_conn()
+        #elif self.ex_type == "direct":
+        #    self._init_direct_conn()
+        elif not blocking:
+            self._init_topic_conn()
+        else:
+            raise NotImplementedError("Only 'topic' and 'direct' exchange types are currently supported.")
+
+
+    def _init_topic_conn(self):
+        self.connection = SelectConnection(self.conn_params, self.on_connected)
+        try:
+            # Loop so we can communicate with RabbitMQ
+            self.connection.ioloop.start()
+        except KeyboardInterrupt:
+            # Gracefully close the connection
+            self.connection.close()
+            # Loop until we're fully closed, will stop on its own
+            self.connection.ioloop.start()
+        else:
+            if DEBUG: print "Connection attempt rejected. Goodbye."
+
+
+    def _init_direct_conn(self):
+        blocking = True
+        self.connection = BlockingConnection(self.conn_params)
+        self.on_connected(self.connection, blocking)
+        self.channel = self.connection.channel()
+        self.on_channel_open(self.channel)
+
+
+    def on_connected(self, connection, blocking=False):
+        '''
+        '''
+        if DEBUG: print self._debug_prefix + "Connected\n  Host: %s\n  Exchange: %s\n" % (self.conn_params.host, self.exchange) + self._debug_prefix + "Creating channel...",
+        # These should always be the same, but just in case...
+        if self.connection is not connection:
+            # Adopt the new connection object
+            self.connection = connection
+        if not blocking: self.connection.channel(on_open_callback=self.on_channel_open)
+
+
+    def on_channel_open(self, new_channel):
+        '''
+        '''
+        if DEBUG: print "Created\n" + self._debug_prefix + "Declaring %s exchange: %s" % (self.ex_type, self.exchange)
+        self.channel = new_channel
+        self.channel.exchange_declare(exchange=self.exchange, type=self.ex_type, callback=self.init_callback)
+
+
+    def send_message(self, body, routing_key=None):
+        '''
+        Sends a message to the exchange on the server to which a connection 
+        has already been established. Both parameters 'body' and 'routing_key' 
+        must be of type str, or a TypeError will be raised. If no routing_key
+        is specified, it defaults to the routing key value specified at 
+        declaration.
+        '''
+        if type(body) != str:
+            raise TypeError("Parameter 'body' must be of type 'str', got '%s' instead." % type(body))
+        if routing_key == None:
+            routing_key = self.routing_key
+        if type(routing_key) != str:
+            raise TypeError("Parameter 'routing_key' must be of type 'str', got '%s' instead." % type(routing_key))
+
+        #if INFO: print "\n" + self._debug_prefix + "Sending message on %s : %s : %s" % (self.exchange, self.queue_name, routing_key)
+        # TODO: In the following, create a means of catching "unroutable" messages (necessary because the 'mandatory'
+        # flag is set)
+        self.channel.basic_publish(exchange=self.exchange, routing_key=routing_key, body=body, mandatory=True,
+                                   properties=pika.BasicProperties(delivery_mode=2, # Persistent messages
+                                                                   )
+                                   )
+
+
+    def create_queue(self, durable=True, exclusive=False, name=None, callback=None):
+        '''
+        Typically don't change the default parameters.
+        '''
+        if DEBUG: print self._debug_prefix + "Creating queue..."
+        if not name:
+            name = self.queue_name
+        self.queue_created = True
+        self.channel.queue_declare(durable=durable, exclusive=exclusive, queue=name, callback=callback)
+
+
+    def bind_routing_key(self, frame):
+        if DEBUG: print self._debug_prefix + "Binding queue '%s' to key '%s'" % (self.queue_name, self.routing_key)
+        self.binding_key = self.routing_key # Signals that we've performed the queue binding
+        self.channel.queue_bind(exchange=self.exchange, queue=self.queue_name, routing_key=self.binding_key, callback=self._rcv())
+
+
+    def receive_message(self, callback=None, no_ack=False, frame=None):
+        '''
+        Retrieves a message from the channel and queue specified in earlier 
+        initialization code. The callback function defaults to 
+        self.process_msg. Only under rare circumstances should no_ack be set 
+        to True, since this will not inform RabbitMQ that completed tasks can 
+        be removed from the queue.
+        '''
+
+        if DEBUG: print self._debug_prefix + "Preparing to receive"
+
+        # Set the callback function for processing received messages
+        if callback == None:
+            self.rcv_callback = self.process_msg
+        else:
+            self.rcv_callback = callback
+        self.no_ack = no_ack
+
+        # Check the queue, declare if necessary
+        if not self.queue_created:
+            self.create_queue(callback=self.bind_routing_key)
+
+        # Check the binding key, bind the channel if necessary
+        elif self.binding_key == None:
+            if frame == None: raise TypeError("Parameter 'frame' must not be None if the routing key has not yet been bound.")
+            self.bind_routing_key(frame) # self._rcv() is specified as the callback in this method
+
+        else:
+            self._rcv()
+
+
+    def _rcv(self):
+        self.channel.basic_qos(prefetch_count=1)
+        if DEBUG: print self._debug_prefix + "Consuming..."
+        self.channel.basic_consume(consumer_callback=self.rcv_callback, queue=self.queue_name, no_ack=self.no_ack)
+
+
+    def ack(self, tag):
+        self.channel.basic_ack(delivery_tag=tag)
+
+
+    def reject(self, tag):
+        self.channel.basic_reject(delivery_tag=tag)
+
+
+    def process_msg(self, channel, method, header, body):
+        raise NotImplementedError
+
+
+
+class PendSend(object):
+    '''
+    '''
+
+    class _QueueChecker(threading.Thread):
+        def __init__(self, output_queue):
+            pass
+
+    def __init__(self):
+        pass
+
+    def get_bindings(self, host, exchange, queue, routing_key):
+        # TODO: Check that all parameters are strings
+        url = "http://guest:guest@localhost:55672/api/bindings"
+        fin = urllib.urlopen(url)
+        result = json.loads(fin.read())
+        fin.close()
+        for r in result:
+            if r['source'] == 'cuff_link_exchange' and r['destination'] == 'cuff_link_queue' and r['routing_key'] == '#':
+                return True
+        return False
--- bullitt/server/db.py
+++ bullitt/server/db.py
@@ -11,17 +11,51 @@ For documentation on SQLAlchemy, visit:
 # Library imports
 import hashlib
 import json
+import os
 import uuid
 
 # Third-party libraries
 from sqlalchemy import create_engine, MetaData, Table, Column, String, \
-                       Integer, Boolean
+                       Integer, Boolean, and_
+from sqlalchemy.sql import select
 
 # Local imports
 
 # Constants
 __all__ = ['ServerBiz']
-LOGGING = True
+LOGGING = False
+
+
+
+def get_hash(obj, hashalg='sha1'):
+    '''
+    Return the hash of the passed object.
+    
+    :param obj:
+    Can be either a string or file object.  If obj is a str, tries to open it
+    as a file first and hash the contents of the file.  If that fails, the 
+    string itself is hashed.  If obj is a file, the file is read and hashed.
+    
+    :param hashalg:
+    Specifies the hashing algorithm that should be used on obj.  Must be an
+    algorithm provided by hashlib.  Defaults to 'sha1'.
+    '''
+    if type(obj) == str or type(obj) == unicode:
+        try:
+            with open(obj) as fin:
+                return hashlib.__dict__[hashalg](fin.read())
+        except IOError:
+            return hashlib.__dict__[hashalg](obj)
+    elif type(obj) == file:
+        return hashlib.__dict__[hashalg](file.read())
+
+
+def get_new_id():
+    '''
+    Return a new UUID.
+    '''
+    return uuid.uuid4()
+
 
 
 class ServerBiz(object):
@@ -55,11 +89,16 @@ class ServerBiz(object):
         pass
     
     
-    def get_file_peers(self, file_id):
+    def get_file_peers(self, file_id, client_id):
         '''
         For a given file, return the list of clients with a copy of it.
+        
+        Does not return the result if the client does not have read or write
+        permissions on the file.
         '''
-        pass
+        # Check the client's permissions
+        
+        # If the client has read or write permissions, select all the peers
     
     
     def update_file(self, file_id, client_id, op, info):
@@ -141,22 +180,48 @@ class BullittSQL(object):
 
     def __init__(self):
         '''
+        Constructor.
+        
+        Initializes the configuration of the object from server_config.json.
+        Creates an engine object and makes a connection to the database.  Forms
+        a metadata object and calls create_tables().
         '''
-        with open('server_config.json') as fin:
+        # Get the path to where this file is stored - Prevents an error when 
+        # opening the JSON config file.
+        p = os.path.dirname(os.path.realpath(__file__))
+        with open(os.path.join(p, 'server_config.json')) as fin:
             config = json.load(fin)
         
         self.metadata = MetaData()
-        self.metadata.bind = create_engine('%s%s://%s:%s@%s/%s' % \
-                                           (config['db_type'],
-                                            config['db_conn'] != '' and \
-                                            '+' + config['db_conn'] or '',
-                                            config['sql_user'],
-                                            config['sql_pass'],
-                                            config['db_url'],
-                                            config['db_name']),
-                                           echo=LOGGING)
-        self.file_table = None
-        self.perm_table = None
+        self.engine = create_engine('%s%s://%s:%s@%s/%s' % \
+                                    (config['db_type'],
+                                     config['db_conn'] != '' and \
+                                     '+' + config['db_conn'] or '',
+                                     config['sql_user'],
+                                     config['sql_pass'],
+                                     config['db_url'],
+                                     config['db_name']),
+                                    echo=LOGGING)
+        self.conn = self.engine.connect()
+        self.metadata.bind = self.engine
+        
+        # Define the tables for the database
+        self.file_table = Table('file_list', self.metadata,
+                                Column('file_id', String(36), primary_key=True,
+                                       nullable=False),
+                                Column('file_name', String(128),
+                                       nullable=False),
+                                Column('owner_id', String(36), nullable=False),
+                                Column('sha1', String(40), nullable=False),
+                                Column('size', Integer, nullable=False))
+        self.perm_table = Table('permissions', self.metadata,
+                                Column('file_id', String(36), primary_key=True),
+                                Column('user_id', String(36), primary_key=True),
+#                                Column('user_key', String(), nullable=False),
+                                Column('read', Boolean, nullable=False),
+                                Column('write', Boolean, nullable=False),
+                                Column('owner', Boolean, nullable=False))
+        
         self.create_tables()
     
     
@@ -168,29 +233,153 @@ class BullittSQL(object):
         Defaults to False, drop the tables before adding them, effectively 
         resetting the entire database.
         '''
-        
-        # Define the tables for the database
-        self.file_table = Table('file_list', self.metadata,
-                                Column('file_id', String(36), primary_key=True,
-                                       nullable=False),
-                                Column('file_name', String(128),
-                                       nullable=False),
-                                Column('owner_id', String(36), nullable=False),
-                                Column('sha1', String(40), nullable=False))
-        self.perm_table = Table('permissions', self.metadata,
-                                Column('pk', Integer, primary_key=True),
-                                Column('file_id', String(36), nullable=False),
-                                Column('user_id', String(36), nullable=False),
-                                Column('read', Boolean, nullable=False),
-                                Column('write', Boolean, nullable=False),
-                                Column('owner', Boolean, nullable=False))
         if drop:
             # Drop all the tables associated with the metadata object
-            self.metadata.drop_all()
+            self.file_table.drop(self.engine)
+            self.perm_table.drop(self.engine)
         # Create the tables
         self.metadata.create_all()
+    
+    
+    def insert_file(self, file_id, client_id, file_name, sha1, size):
+        '''
+        Insert the data for a new file.
+        
+        Returns a 2-tuple with the results from the insert statements to the
+        file_list and permissions table, respectively.
+        '''
+        try:
+            # If we were passed a hash object, get the hex digest string
+            sha1 = sha1.hexdigest()
+        except AttributeError:
+            pass
+        fvals = dict(file_id=file_id,
+                     file_name=file_name,
+                     owner_id=client_id,
+                     sha1=sha1,
+                     size=size,
+                     )
+        pvals = dict(file_id=file_id,
+                     user_id=client_id,
+                     read=True,
+                     write=True,
+                     owner=True,
+                     )
+        fres = self.file_table.insert().execute(**fvals)
+        pres = self.perm_table.insert().execute(**pvals)
+        return fres, pres
+    
+    
+    def select_file(self, file_id, field=None):
+        '''
+        Retrieve the file entry corresponding to the given file_id.
+        
+        :param field:
+        To retrieve a specific field of a file entry, specify it here.  
+        Defaults to None, which gets all fields.
+        
+        Entry fields are returned as a dictionary with the names of the fields
+        as the keys.
+        '''
+        if field == None:
+            field = [self.file_table]
+        else:
+            field = [self.file_table.c.__dict__[field]]
+        result = self.conn.execute(select(field))
+        row = result.fetchone()
+        result.close
+        keys = tuple([col.name for col in tuple(self.file_table.columns)])
+        return dict(zip(keys, row))
+    
+    
+    def update_file(self, file_id, sha1, size):
+        try:
+            # If we were passed a hash object, get the hex digest string
+            sha1 = sha1.hexdigest()
+        except AttributeError:
+            pass
+        fvals = dict(sha1=sha1, size=size)
+        ret1 = self.file_table.update(self.file_table.c.file_id == str(file_id))
+        return ret1.execute(**fvals)
+    
+    
+    def delete_file(self, file_id):
+        '''
+        Delete the file entry matching the given ID.
+        '''
+        ret1 = self.file_table.delete(self.file_table.c.file_id == file_id)
+        ret1 = ret1.execute()
+        
+        ret2 = self.perm_table.delete(self.perm_table.c.file_id == file_id)
+        ret2.execute()
+        return ret1, ret2
+    
+    
+    def insert_perm(self, file_id, client_id, read=0, write=0):
+        '''
+        Insert a new set of permissions for a client on a file. 
+        '''
+        if (read, write) == (False, False):
+            raise ValueError('Parameter read or write must be True or 1.')
+        
+        pvals = dict(file_id=file_id,
+                     user_id=client_id,
+                     read=bool(read),
+                     write=bool(write),
+                     owner=False,
+                     )
+        return self.perm_table.insert().execute(**pvals)
+    
+    
+    def select_perm(self, file_id, client_id):
+        '''
+        Return the permissions of the given client on the given file.
+        
+        Permissions are returned as a tuple of 3 boolean values:
+        (read, write, owner)
+        '''
+        s = select([self.perm_table.c.read,
+                    self.perm_table.c.write,
+                    self.perm_table.c.owner],
+                   and_(self.perm_table.c.file_id == file_id,
+                        self.perm_table.c.user_id == client_id)
+                   )
+        result = self.conn.execute(s)
+        row = result.fetchone()
+        result.close()
+        return row
+    
+    
+    def update_perm(self, file_id, client_id, read=None, write=None):
+        '''
+        For an existing record, change the permissions for a client on a file.
+        '''
+        if (read, write) == (None, None):
+            raise ValueError('Parameter read or write must be specified.')
+        
+        pvals = dict()
+        if read is not None:
+            pvals['read'] = bool(read)
+        if write is not None:
+            pvals['write'] = bool(write)
+        
+        ret1 = self.perm_table.update(and_(
+                                        self.perm_table.c.file_id == file_id,
+                                        self.perm_table.c.user_id == client_id))
+        return ret1.execute(**pvals)
+    
+    
+    def delete_perm(self, file_id, client_id):
+        '''
+        Delete the permission entry matching the given file and user IDs.
+        '''
+        ret1 = self.perm_table.delete(and_(
+                                       self.perm_table.c.file_id == file_id,
+                                       self.perm_table.c.user_id == client_id))
+        return ret1.execute()
 
 
+    
 
 
 
