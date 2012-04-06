2cc01d3e8c4c6c1391e51708f142ae2d94e4881a
burdenedreflect <jpaglier@asu.edu>
2012-04-05 17:24:47 -0700

    Keep stubbing, add shebangs. Inc commit.

--- bullitt/client/deamon.py
+++ bullitt/client/deamon.py
@@ -1,8 +1,35 @@
+#!/usr/bin/python
+
 '''
 Created on Apr 4, 2012
 
 @author: Justin
 '''
+
+import subprocess
+import hashlib
+import uuid
+import Crypto #this isnt resolving in eclipse but works command line
+
+def choochoo():
+    #learned to use subprocess left it because sl is wonderful
+    subprocess.call("sl")
+    
+def split_file():
+    '''
+    Calls dd to split a file into
+    the number of files we decide to split
+    '''
+    
+def reassemble_slices():
+    '''
+    Decrypt slices and put them back together
+    '''
+    
+def decrypt_slice():
+    '''
+    Decrypt slice duhhh
+    '''
 
 if __name__ == '__main__':
     pass
