31e572c83ee0b2f70bc0da1cfc28807c7186b7a4
burdenedreflect <jpaglier@asu.edu>
2012-04-09 15:42:01 -0700

    Split exception handling added

--- bullitt/client/daemon.py
+++ bullitt/client/daemon.py
@@ -9,17 +9,35 @@ Created on Apr 4, 2012
 import subprocess
 import hashlib
 import uuid
+import os
+import math
 from Crypto import *
+
+CONST_SLICE_SIZE = 180000 #180KB slice size
 
 def choochoo():
     #learned to use subprocess left it because sl is wonderful
     subprocess.call("sl")
     
-def split_file():
+def slice_file(file, slice_number):
     '''
-    Calls dd to split a file into
-    the number of files we decide to split
+    Calls dd to split a file into 180KB blocks
+    The slice argument for this slice size comes from
+    http://www.unitethecows.com/p2p-general-discussion/30807-rodi-anonymous-p2p.html#post203703
     '''
+    
+    try:
+        file_size = os.path.getsize(file)
+        start_byte = CONST_SLICE_SIZE * slice_number
+        end_byte = start_byte + CONST_SLICE_SIZE - 1
+    
+        if start_byte < file_size:
+            #print "dd if={0} of={0}.slice{1} ibs={2} skip={1} count=1".format(file, slice_number, CONST_SLICE_SIZE)
+            subprocess.call("dd if={0} of={0}.slice{1} ibs={2} skip={1} count=1".format(file, slice_number, CONST_SLICE_SIZE))
+        else:
+            print "Slice is outside of file!!"
+    except OSError:
+        print "File not found or other OSError"
     
 def reassemble_slices():
     '''
@@ -36,5 +54,13 @@ def create_session_key():
     Create a session key
     '''
 
+def send_slice():
+    '''
+    Send slice to a vm
+    '''
+    
 if __name__ == '__main__':
+    #num_slices = int(math.ceil(os.path.getsize("C:/Users/Justin/Desktop/trying/Paper-5(1).pdf") / float(CONST_SLICE_SIZE)))
+    #for x in range(num_slices):
+    slice_file("C:/Users/Justin/Desktop/trying/Paper-5(1).pdf", 0)
     pass
